77bfd174c2971a17aff25447b9832263594064f6
xf00889 <hutchiejn@gmail.com>
2025-06-21 02:20:31 +0800

    refinement

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 # Install Python dependencies
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install --timeout=100 -r requirements-deploy.txt
 
 # Collect static files
 python manage.py collectstatic --noinput 
